cc8e47414a1a60bc6648170c012100da14f13747
root <110252997+poyws@users.noreply.github.com>
2024-11-05 15:00:02 -0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'estoque_db');
+define('DB_USER', 'teste');
+define('DB_PASS', '');
+
+define('BASE_URL', 'http://localhost/estoque-app/public/'); //seu db
+?>
