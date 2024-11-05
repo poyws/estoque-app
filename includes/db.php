3ee6011b50b2cdcb24d02b8019e3287e74efa71d
root <110252997+poyws@users.noreply.github.com>
2024-11-05 15:00:24 -0300

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php
+// Conexão com o banco de dados, coloque o seu.
+function dbConnect() {
+    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    try {
+        return new PDO($dsn, DB_USER, DB_PASS, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}
+?>
